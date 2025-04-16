3c4c0f35a059de1290aadd782d68bd4abfafa5bd
Varsha-Nayak04 <kvarshanayak@gmail.com>
2025-04-17 04:05:24 +0530

    first commit

---
+++ src/main/java/MavenTestProject1010/MavenProject1010/App.java
@@ -0,0 +1,13 @@
+package MavenTestProject1010.MavenProject1010;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
